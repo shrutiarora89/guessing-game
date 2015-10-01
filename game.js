08420b4aa381d489fc22b0aa11aa21056e1a187d
Aaron Filson <aaronfilson@gmail.com>
2015-10-01 14:44:56 -0700

    Move logic into functionsv2

--- game.js
+++ game.js
@@ -1,47 +1,63 @@
 var count = 0;
 
 // The Three Questions
-var question1 = prompt('Do you learn during the lab time?');
-
 //////////////////////////////////////////////////////////////////////////////////////
-if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
-    alert('Great!');
-    count = count + 1;
-} else if (question1.toUpperCase() === 'NO' || question1.toUpperCase() === 'N') {
-    alert('No, That\'s not the right answer');
-} else {
-    alert('Plz provide a relevant response.');
-}
+
+function question1func() {
+
+    var question1 = prompt('Do you learn during the lab time?');
+
+    if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
+        alert('Great!');
+        count = count + 1;
+    } else if (question1.toUpperCase() === 'NO' || question1.toUpperCase() === 'N') {
+        alert('No, That\'s not the right answer');
+    } else {
+        alert('Plz provide a relevant response.');
+    }
+
 
 // Taking the Answer and Logging it with a meaning message
 console.log('This is what you answered for Question1 ' + question1.toUpperCase());
-//////////////////////////////////////////////////////////////////////////////////////
-var question2 = prompt('Is it raining today?');
-
-if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
-    alert('Lucky you');
-    count = count + 1;
-} else if (question2.toUpperCase() === 'NO' || question2.toUpperCase() === 'N') {
-    alert('No, That\'s not the right answer');
-} else {
-    alert('Please Provide a relevant response.');
 }
 
-console.log('This is what you answered for Question2 ' + question2.toUpperCase());
-
 //////////////////////////////////////////////////////////////////////////////////////
 
-var question3 = prompt('Are you excited to learn more coding?');
+function question2func() {
+    var question2 = prompt('Is it raining today?');
 
-if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
-    alert('Lucky you');
-    count = count + 1;
-} else if (question3.toUpperCase() === 'NO' || question3.toUpperCase() === 'N') {
-    alert('No, That\'s not the right answer');
-} else {
-    alert('Please Provide a relevant response.');
+    if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
+        alert('Lucky you');
+        count = count + 1;
+    } else if (question2.toUpperCase() === 'NO' || question2.toUpperCase() === 'N') {
+        alert('No, That\'s not the right answer');
+    } else {
+        alert('Please Provide a relevant response.');
+    }
+
+    console.log('This is what you answered for Question2 ' + question2.toUpperCase());
 }
 //////////////////////////////////////////////////////////////////////////////////////
+
+function question3func() {
+    var question3 = prompt('Are you excited to learn more coding?');
+
+    if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
+        alert('Lucky you');
+        count = count + 1;
+    } else if (question3.toUpperCase() === 'NO' || question3.toUpperCase() === 'N') {
+        alert('No, That\'s not the right answer');
+    } else {
+        alert('Please Provide a relevant response.');
+    }
+    console.log('This is what you answered for Question3 ' + question3.toUpperCase());
+
+}
+//////////////////////////////////////////////////////////////////////////////////////
+question1func();
+question2func();
+question3func();
+
 
 //Count the Correct answers
 alert('Hi!! You have got,' + count + ' right answers!!Thanks for Playing.');
