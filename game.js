e10afc6b07bc4cde10ef95a5a8c367eee48ff0c2
Shruti Arora <shruti.arora89@icloud.com>
2015-09-30 22:54:14 -0700

    second commit: error correction logic

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
+
 // The Three Questions
 var question1 = prompt('Do you learn during the lab time?');
 
 //////////////////////////////////////////////////////////////////////////////////////
-if(question1 === 'YES'){
+if(question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y'){
 	alert('Great!');
 }
-else {
+else if(question1.toUpperCase() === 'NO' || question1.toUpperCase() === 'N'){
 	alert('No, That\'s not the right answer');
+}
+else {
+	alert('Plz provide a relevant response.');
 }
 
 // Taking the Answer and Logging it with a meaning message
@@ -14,11 +18,14 @@ console.log('This is what you answered for Question1 ' + question1.toUpperCase()
 //////////////////////////////////////////////////////////////////////////////////////
 var question2 = prompt('Is it raining today?');
 
-if(question2 === 'YES'){
+if(question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y'){
 	alert('Lucky you');
 }
-else {
+else if(question2.toUpperCase() === 'NO' || question2.toUpperCase() === 'N'){
 	alert('No, That\'s not the right answer');
+}
+else {
+	alert('Please Provide a relevant response.');
 }
 
 console.log('This is what you answered for Question2 ' + question2.toUpperCase());
@@ -27,10 +34,15 @@ console.log('This is what you answered for Question2 ' + question2.toUpperCase()
 
 var question3 = prompt('Are you excited to learn more coding?');
 
-if(question3 === 'YES'){
+if(question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y'){
 	alert('Lucky you');
 }
-else {
+else if(question3.toUpperCase() === 'NO' || question3.toUpperCase() === 'N'){
 	alert('No, That\'s not the right answer');
 }
+else {
+	alert('Please Provide a relevant response.');
+}
 //////////////////////////////////////////////////////////////////////////////////////
+
+
