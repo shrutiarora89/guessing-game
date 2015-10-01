3f669e27ef156b4a187bf681a051c51a81eea719
Shruti Arora <shruti.arora89@icloud.com>
2015-09-30 22:17:43 -0700

    The Third Guessing-Game Question

--- game.js
+++ game.js
@@ -36,6 +36,23 @@ console.log('This is what you answered for Question2 ' + question2.toUpperCase()
 
 //////////////////////////////////////////////////////////////////////////////////////
 
+var question3 = prompt('Are you excited to learn more coding?');
+
+if(question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y'){
+	alert('Lucky you');
+	count=count+1;
+}
+else if(question3.toUpperCase() === 'NO' || question3.toUpperCase() === 'N'){
+	alert('No, That\'s not the right answer');
+}
+else {
+	alert('Please Provide a relevant response.');
+}
+//////////////////////////////////////////////////////////////////////////////////////
+
+//Count the Correct answers
+alert('Hi!! You have got,' + count +' right answers!!Thanks for Playing.');
+
 
 
 
