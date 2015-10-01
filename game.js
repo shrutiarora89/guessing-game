07d17c4c5329f6c10c4e77b9f8be3be213b890e5
Shruti Arora <shruti.arora89@icloud.com>
2015-09-30 22:53:31 -0700

    first commit: if/else logic

---
+++ game.js
@@ -0,0 +1,36 @@
+// The Three Questions
+var question1 = prompt('Do you learn during the lab time?');
+
+//////////////////////////////////////////////////////////////////////////////////////
+if(question1 === 'YES'){
+	alert('Great!');
+}
+else {
+	alert('No, That\'s not the right answer');
+}
+
+// Taking the Answer and Logging it with a meaning message
+console.log('This is what you answered for Question1 ' + question1.toUpperCase());
+//////////////////////////////////////////////////////////////////////////////////////
+var question2 = prompt('Is it raining today?');
+
+if(question2 === 'YES'){
+	alert('Lucky you');
+}
+else {
+	alert('No, That\'s not the right answer');
+}
+
+console.log('This is what you answered for Question2 ' + question2.toUpperCase());
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+var question3 = prompt('Are you excited to learn more coding?');
+
+if(question3 === 'YES'){
+	alert('Lucky you');
+}
+else {
+	alert('No, That\'s not the right answer');
+}
+//////////////////////////////////////////////////////////////////////////////////////
