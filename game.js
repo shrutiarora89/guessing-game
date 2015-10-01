943754c8db3da5e0e8b2b02d01f64fb166a4916a
Shruti Arora <shruti.arora89@icloud.com>
2015-09-30 22:54:43 -0700

    third commit: summary logic

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+var count = 0;
 
 // The Three Questions
 var question1 = prompt('Do you learn during the lab time?');
@@ -5,6 +6,7 @@ var question1 = prompt('Do you learn during the lab time?');
 //////////////////////////////////////////////////////////////////////////////////////
 if(question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y'){
 	alert('Great!');
+	count=count+1;
 }
 else if(question1.toUpperCase() === 'NO' || question1.toUpperCase() === 'N'){
 	alert('No, That\'s not the right answer');
@@ -20,6 +22,7 @@ var question2 = prompt('Is it raining today?');
 
 if(question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y'){
 	alert('Lucky you');
+	count=count+1;
 }
 else if(question2.toUpperCase() === 'NO' || question2.toUpperCase() === 'N'){
 	alert('No, That\'s not the right answer');
@@ -36,6 +39,7 @@ var question3 = prompt('Are you excited to learn more coding?');
 
 if(question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y'){
 	alert('Lucky you');
+	count=count+1;
 }
 else if(question3.toUpperCase() === 'NO' || question3.toUpperCase() === 'N'){
 	alert('No, That\'s not the right answer');
@@ -44,5 +48,10 @@ else {
 	alert('Please Provide a relevant response.');
 }
 //////////////////////////////////////////////////////////////////////////////////////
+
+//Count the Correct answers
+alert('Hi!! You have got,' + count +' right answers!!Thanks for Playing.');
+
+
 
 
